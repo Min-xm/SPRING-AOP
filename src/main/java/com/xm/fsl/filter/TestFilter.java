110fa86c1c9cab114858535b824372d3da8c7098
Min-xm <2998275895@qq.com>
2021-05-11 15:28:19 +0800

    拦截器详细

--- src/main/java/com/xm/fsl/filter/TestFilter.java
+++ src/main/java/com/xm/fsl/filter/TestFilter.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 //过滤器(Filter)：它依赖于servlet容器。在实现上，基于函数回调，它可以对几乎所有请求进行过滤，
 //但是缺点是一个过滤器实例只能在容器初始化时调用一次。使用过滤器的目的，是用来做一些过滤操作，获
-//取我们想要获取的数据，比如：在Javaweb中，对传入的request、response提前过滤掉一些信息，或
+//取我们想要获取的数据，比如：在JavaWeb中，对传入的request、response提前过滤掉一些信息，或
 //者提前设置一些参数，然后再传入servlet或者Controller进行业务逻辑操作。通常用的场景是：在过
 //滤器中修改字符编码（CharacterEncodingFilter）、在过滤器中修改HttpServletRequest的一
 //些参数（XSSFilter(自定义过滤器)），如：过滤低俗文字、危险字符等。
